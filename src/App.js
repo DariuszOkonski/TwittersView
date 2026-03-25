a84b628275add076fd4f0c3832e28a4416938eba
Dariusz Okoński <darek200180@gmail.com>
2026-03-25 06:17:27 +0100

    initialization

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListWrapper from './components/ListWrapper/ListWrapper';
+import './index.css';
+
+function App() {
+  return (
+    <div className='wrapper'>
+      <ListWrapper />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ListWrapper/ListWrapper.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ListWrapper = () => {
+  return (
+    <ul>
+      <li>List item 1</li>
+      <li>List item 2</li>
+      <li>List item 3</li>
+    </ul>
+  );
+};
+
+export default ListWrapper;
