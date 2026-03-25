03d3c9afcfd34542ebf755e887aab58bd7612914
Dariusz Okoński <darek200180@gmail.com>
2026-03-25 06:35:53 +0100

    add basic styles

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './ListItem.css';
+import danAbramowImage from '../../../assets/images/danabramov.jpg';
 
 const ListItem = () => {
-  return <li className='listItemWrapper'>item1</li>;
+  return (
+    <li className='listItem__wrapper'>
+      <img src={danAbramowImage} className='listItem__image' />
+      <div>
+        <h2 className='listItem__name'>Dan Abramov</h2>
+        <p className='listItem__description'>
+          It is a good idea to think of your PC as an office. It stores files,
+          programs, pictures. This can be compared to an actual office's files,
+          machines and decorations.
+        </p>
+        <button className='listItem__button'>visit twitter page</button>
+      </div>
+    </li>
+  );
 };
 
 export default ListItem;
