298ce6260fd3f4003bb82867132c9fea594ed046
Dariusz Okoński <darek200180@gmail.com>
2026-04-14 07:47:18 +0200

    add default picture

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import ListWrapper from './components/ListWrapper/ListWrapper';
 import './index.css';
 import { twitterAccounts } from './data/twitterAccounts';
 import Form from './components/ListWrapper/Form/Form';
+import defaultPicture from './assets/images/defaultPicture.jpg';
 
 const initialStateItems = [...twitterAccounts];
+
+const checkIfPicture = (picture) => {
+  const extensions = ['.jpg', '.png'];
+  return extensions.some((ext) => picture.includes(ext))
+    ? picture
+    : defaultPicture;
+};
 
 class App extends React.Component {
   state = {
@@ -13,7 +21,17 @@ class App extends React.Component {
 
   addItem = (e) => {
     e.preventDefault();
-    console.log(e.target);
+
+    const newItem = {
+      name: e.target[0].value,
+      twitterLink: e.target[1].value,
+      image: checkIfPicture(e.target[2].value),
+      description: e.target[3].value,
+    };
+
+    this.setState({
+      items: [...this.state.items, newItem],
+    });
   };
 
   render() {
