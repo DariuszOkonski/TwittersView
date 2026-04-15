cbd9a215a1b15b4da26039276c55b14f18d99d9e
Dariusz Okoński <darek200180@gmail.com>
2026-04-15 07:26:04 +0200

    update form module scss

--- src/components/ListWrapper/Form/Form.js
+++ src/components/ListWrapper/Form/Form.js
@@ -7,28 +7,54 @@ const Form = ({ submitFn }) => (
     <form className={styles.form} onSubmit={submitFn}>
       <div className={styles.item}>
         <input
+          className={styles.input}
           type='text'
           name='name'
           id='name'
           placeholder=' '
           maxLength='30'
         />
-        <label htmlFor='name'>Name</label>
+        <label className={styles.label} htmlFor='name'>
+          Name
+        </label>
         <div className={styles.itemBar} />
       </div>
       <div className={styles.item}>
-        <input type='text' name='link' id='link' placeholder=' ' />
-        <label htmlFor='link'>Link</label>
+        <input
+          className={styles.input}
+          type='text'
+          name='link'
+          id='link'
+          placeholder=' '
+        />
+        <label className={styles.label} htmlFor='link'>
+          Link
+        </label>
         <div className={styles.itemBar} />
       </div>
       <div className={styles.item}>
-        <input type='text' name='image' id='image' placeholder=' ' />
-        <label htmlFor='image'>Image</label>
+        <input
+          className={styles.input}
+          type='text'
+          name='image'
+          id='image'
+          placeholder=' '
+        />
+        <label className={styles.label} htmlFor='image'>
+          Image
+        </label>
         <div className={styles.itemBar} />
       </div>
       <div className={styles.item}>
-        <textarea name='description' id='description' placeholder=' ' />
-        <label htmlFor='description'>Description</label>
+        <textarea
+          className={styles.textarea}
+          name='description'
+          id='description'
+          placeholder=' '
+        />
+        <label className={styles.label} htmlFor='description'>
+          Description
+        </label>
         <div className={styles.itemBar} />
       </div>
       <button className={styles.button} type='submit'>
