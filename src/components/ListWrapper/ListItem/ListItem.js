236c9df7622b151b404bcde2b6c0070510eb26cc
Dariusz Okoński <darek200180@gmail.com>
2026-05-12 05:34:49 +0200

    add Title component

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ListItem.module.scss';
 import Button from '../../Button/Button';
+import Title from '../../Title/Title';
 
 const ListItem = ({ name, description, image, twitterLink }) => {
   const ImageTag = image ? 'img' : 'div';
@@ -14,7 +15,7 @@ const ListItem = ({ name, description, image, twitterLink }) => {
         alt={name}
       />
       <div>
-        <h2 className={styles.name}>{name}</h2>
+        <Title>{name}</Title>
         <p className={styles.description}>{description}</p>
 
         <Button href={twitterLink}>visit twitter page</Button>
