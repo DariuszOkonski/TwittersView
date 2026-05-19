3e93de2572b022c710fbd4f4758e4f535edfb374
Dariusz Okoński <darek200180@gmail.com>
2026-05-19 06:02:01 +0200

    install react-router in version 5 and basic components

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './index.module.scss';
 import { twitterAccounts } from '../../data/twitterAccounts';
+import { BrowserRouter, Route } from 'react-router-dom';
+import TwittersView from '../TwittersView/TwittersView';
+import ArticlesView from '../ArticlesView/ArticlesView';
+import NotesView from '../NotesView/NotesView';
 
 const initialStateItems = [...twitterAccounts];
 
@@ -36,9 +40,13 @@ class Root extends React.Component {
 
   render() {
     return (
-      <div className='wrapper'>
-        <h1>Hello world</h1>
-      </div>
+      <BrowserRouter>
+        <React.Fragment>
+          <Route exact path='/' component={TwittersView} />
+          <Route path='/articles' component={ArticlesView} />
+          <Route path='/notes' component={NotesView} />
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
