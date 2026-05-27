d437c95706d6504bf38a39d049a02971cacd1748
Dariusz Okoński <darek200180@gmail.com>
2026-05-27 06:28:08 +0200

    add noItems

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,11 +4,19 @@ import styles from './List.module.scss';
 
 const ListWrapper = ({ items }) => {
   return (
-    <ul className={styles.wrapper}>
-      {items.map((item) => (
-        <ListItem key={crypto.randomUUID()} {...item} />
-      ))}
-    </ul>
+    <React.Fragment>
+      {items.length ? (
+        <ul className={styles.wrapper}>
+          {items.map((item) => (
+            <ListItem key={crypto.randomUUID()} {...item} />
+          ))}
+        </ul>
+      ) : (
+        <h1 className={styles.noItems}>
+          There's nothing here yet, please add some items!
+        </h1>
+      )}
+    </React.Fragment>
   );
 };
 
